2e652f611dfab3432c1649eab32e2034fe0c3222
Ruslan6877 <rusla.n775809@gmail.com>
2024-02-10 16:35:41 +0500

    footer page end

--- javascript/carusel.js
+++ javascript/carusel.js
@@ -1,6 +1,6 @@
 $('.owl-carousel').owlCarousel({
     loop: true,
-    margin: 40,
+    
     nav: false,
     autoplay: true,
     autoWidth: true,
@@ -10,11 +10,38 @@ $('.owl-carousel').owlCarousel({
         0: {
             items: 1
         },
-        600: {
+        300:{
+            items: 2
+        },
+        500: {
             items: 3
+        },
+        700:{
+            items: 4
         },
         1000: {
             items: 5
         }
     }
 })
+
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     responsiveClass:true,
+//     responsive:{
+//         0:{
+//             items:1,
+//             nav:true
+//         },
+//         600:{
+//             items:3,
+//             nav:false
+//         },
+//         1000:{
+//             items:5,
+//             nav:true,
+//             loop:false
+//         }
+//     }
+// })
